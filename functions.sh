2833024b363de53f2107a9d55f6254d50f45997c
James Solomon <solomonjames@spotify.net>
2014-06-03 21:49:00 +0000

    Adding some new bash stuff

---
+++ functions.sh
@@ -0,0 +1,59 @@
+# Setup the dotfiles repo locally, or pull latest version from github.
+# Create symlinks in the $HOME directory to elements in the repo
+jpull() {
+    local REPO='https://github.com/solomonjames/dotfiles'
+    local SGITURL="${REPO}/raw/e374d0dbc1754b21a3d36b9df5742d351d7fe460/git-static-x86_64-linux-musl.tar.xz"
+    local SGITPATH="${HOME}/.git-static"
+    local SGIT=git
+    if ! ${SGIT} --version >/dev/null 2>&1 ; then
+        SGIT="${SGITPATH}/git --exec-path=${SGITPATH}/git-core"
+        if ! ${SGIT} --version >/dev/null 2>&1 ; then
+            cd ${HOME}
+            curl -sL ${SGITURL} | tar -xJf -
+        fi
+    fi
+    if [ -d "${HOME}/.dotfiles" ] ; then
+        cd "${HOME}/.dotfiles"
+        ${SGIT} reset --hard HEAD >/dev/null 2>&1
+        ${SGIT} pull
+        ${SGIT} submodule init
+        ${SGIT} submodule update
+    else
+        cd "${HOME}"
+        ${SGIT} clone --depth 1 ${REPO} .dotfiles
+        cd "${HOME}/.dotfiles"
+        ${SGIT} submodule init
+        ${SGIT} submodule update
+    fi
+    cd "${HOME}/.dotfiles" &&
+    for f in * ; do
+        # A quick check on the files can prevent unnecessary forks
+        lf="${HOME}/.${f%/*}"
+        if [ ! "${f}" -ef "${lf}" ] ; then
+            rm -rf "${lf}"
+            ln -s ".dotfiles/${f}" "${lf}"
+        fi
+    done
+    cd "${HOME}"
+    # Delete any broken symlinks in the homedir
+    find -L . -maxdepth 1 -type l -exec rm -- {} +
+    . "${HOME}/.profile"
+}
+
+# Simple wrapper for ssh which makes jpull() available in the remote session
+# regardless of whether .dotfiles is present remotely or not
+jssh() {
+    local func=$(typeset -f jpull)
+    ssh -A -t "$@" \
+    "${func} ;
+    [ -r /etc/motd ] && cat /etc/motd ;
+    [ -r \"\$HOME/.profile\" ] && . \"\$HOME/.profile\" ;
+    type jssh >/dev/null 2>&1 || jpull ;
+    exec env -i SSH_AUTH_SOCK=\"\$SSH_AUTH_SOCK\" \
+      SSH_CONNECTION=\"\$SSH_CONNECTION\" \
+      SSH_CLIENT=\"\$SSH_CLIENT\" SSH_TTY=\"\$SSH_TTY\" \
+      HOME=\"\$HOME\" TERM=\"\$TERM\" \
+      PATH=\"\$PATH\" SHELL=\"\$SHELL\" \
+      USER=\"\$USER\" \$SHELL -i"
+}    
+alias ssj="jssh"
---
+++ profile
@@ -0,0 +1,40 @@
+hn=$(hostname -s)
+R="\033[00m"
+M="\033[32m"
+P='$'
+
+[ "${USER}" = "root" ] && M="\033[31m" && P='#'
+
+set_ps1() {
+    printf "${hn} ${M}|\033[36m ${PWD}${R}\n${P} "
+}
+
+PS1='$(set_ps1)'
+PS2=" > "
+EDITOR=vim
+TZ=America/New_York
+LANG=en_US.UTF-8
+HISTFILE="${HOME}/.history"
+HISTSIZE=1000
+# Darwinism
+CLICOLOR=1
+
+export EDITOR TZ LANG HISTFILE HISTSIZE CLICOLOR
+
+# Used by bash, unused by mksh
+if [ "${0##*/}" = "bash" ] || [ "${0}" = "-bash" ] ; then
+    shopt -s expand_aliases
+    shopt -s checkwinsize
+    HISTFILESIZE=2000
+    HISTCONTROL=ignoredups:ignorespace
+    export HISTFILESIZE HISTCONTROL
+fi
+
+[ "$(uname -s)" = "Linux" ] && alias ls='ls --color'
+alias lh='ls -lh'
+
+# Helper functions
+[ -r "${HOME}/.functions.sh" ] && . "${HOME}/.functions.sh"
+
+# Load a local profile
+[ -r "${HOME}/.localprofile" ] && . "${HOME}/.localprofile"
