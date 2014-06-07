202e64f74638bfccd3b493d91af6329c2b4a129b
James Solomon <solomonjames@spotify.com>
2014-06-07 12:32:15 -0400

    Adding the fixssh stuff for restoring ssh connection inside a tmux session

---
+++ bash/includes/ssh.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+function ssh-save()
+{
+    SSHVARS="SSH_CLIENT SSH_TTY SSH_AUTH_SOCK SSH_CONNECTION DISPLAY"
+
+    for x in ${SSHVARS} ; do
+        (eval echo $x=\$$x) | sed  's/=/="/
+        s/$/"/
+        s/^/export /'
+    done 1>${HOME}/.fixssh
+}
+
+function ssh-restore()
+{
+    . ${HOME}/.fixssh
+}
